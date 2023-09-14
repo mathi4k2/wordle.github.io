1d83bf5460b73dd6d9bec6c5d061e360ac34e956
mathi4k2 <mathiforcado@gmail.com>
2023-09-13 21:42:44 -0400

    Lista de palabras por API en js

--- script.js
+++ script.js
@@ -1,25 +1,39 @@
-//definicion de cantidad de intentos y palabra a ser adivinada (agregar mas palabras al diccionario de desearse)
+//definicion de cantidad de intentos 
 let intentos = 6;
-let diccionario = ['APPLE', 'HURLS', 'WINGS', 'YOUTH']
-const palabra = diccionario[Math.floor(Math.random() * diccionario.length)];
+
+//definicion de la API
+const API = "https://random-word-api.vercel.app/api?words=1&length=5"
+fetch(API)
+    //solicitar respuesta a la API en formato json
+    .then(response => response.json())
+    //se asigna la respuesta a la variable palabra despues de recibir la respuesta y se la pasa a mayuscula
+    .then((response) => {
+        palabra=response[0].toUpperCase();
+    })
+    //si se obtiene un error en la respuesta de la API se usa palabra aleatoria de un diccionario preestablecido
+    .catch((err)=>{
+        console.log("error en respuesta de la API, se procesa lista de palabras predeterminadas")
+        let diccionario = ['APPLE', 'HURLS', 'WINGS', 'YOUTH']
+        let palabra = diccionario[Math.floor(Math.random() * diccionario.length)];
+    })
 
 //proceso de la funcion init una vez que la ventana carga
 window.addEventListener('load', init);
 
 //definicion de la funcion init
 function init(){
-        console.log('Esto se ejecuta solo cuando se carga la pagina web')
-   
     //declaracion y proceso de la funcion intentar
     function intentar(){
         const INTENTO = leerIntento();
+            //impresion de mensaje en pantalla si la respuesta es correcta
             if (INTENTO === palabra ) {
                 terminar("<h1>GANASTE!😀</h1>")
                 return
             }
+            //impresion del titulo de intentos si la respuesta es incorrecta
             var elemento = document.getElementById('titleintentos');
             elemento.style.display = 'block';
-            
+            //impresion de pista si la respuesta tiene una cantidad de letras diferente a la respuesta
             if (INTENTO.length != palabra.length) {
                 var pista = document.getElementById('pista');
                 pista.style.display = 'block';
@@ -27,6 +41,7 @@ function init(){
             const GRID = document.getElementById("grid");
             const ROW = document.createElement('div');
             ROW.className = 'row';
+            //recorrido de la palabra e impresion en pantalla de la respuesta erronea
             for (let i in INTENTO) {
                 const SPAN = document.createElement('span');
                 SPAN.className = 'letter';
@@ -44,6 +59,7 @@ function init(){
             }
             GRID.appendChild(ROW)
                 intentos--
+            //impresion en pantalla si se llega al limite de intentos (6)
             if (intentos==0){
                 terminar("<h1>PERDISTE!😖</h1>")
             }
@@ -61,12 +77,13 @@ function init(){
         intento = intento.toUpperCase(); 
         return intento;
     }
-    //declaracion y proceso de la funcion terminar
+    //declaracion y proceso de la funcion terminar, poner botn en gris cuando se termina el juego
     function terminar(mensaje){
         const BOTON = document.getElementById("guess-button"); 
         const INPUT = document.getElementById("guess-input");
         INPUT.disabled = true;
         BOTON.disabled = true;
+        BOTON.style.backgroundColor = "gray";
         let contenedor = document.getElementById('guesses');
         contenedor.innerHTML = mensaje;
     }
